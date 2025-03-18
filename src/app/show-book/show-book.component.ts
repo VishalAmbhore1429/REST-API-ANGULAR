3a85bb882f513633acf3eb30ba103f63b4953bc0
Vishal <vishal.ambhore@omunim.com>
2025-03-18 15:12:17 +0530

    Third commit

--- src/app/show-book/show-book.component.ts
+++ src/app/show-book/show-book.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { BookServiceService } from '../Services/book-service.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-book',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './show-book.component.html',
   styleUrl: './show-book.component.css'
 })
 export class ShowBookComponent {
   booksData:any;
   succesMessage:any;
+  showForm : boolean = false;
   getSelectedData:any|undefined;
   bookname:any;
   authorname:any;
@@ -40,13 +42,16 @@ export class ShowBookComponent {
     this.bookservice.getBookData(id).subscribe((data:any)=>{
       console.log(data.bookname);  
       this.getSelectedData = data;
+      this.showForm = true;
     })
   }
 
   updateBook(id:any,data:any){
     this.bookservice.updateBook(id,data).subscribe((result)=>{
       console.log(result);
-      
+      this.succesMessage = 'Book Data Updated Successfully';
+      this.showAllBooks();
+      this.showForm = false;
     })
   }
 }
